2de8596de5b42cc8948481004d5767e4b7f72cff
Tyler Lowrey <contact@tylerlowrey.com>
2023-11-11 22:31:26 -0500

    Refactored, cleaned, and optimized code

--- src/helper.h
+++ src/helper.h
@@ -2,3 +2,4 @@
 
 uint8_t clamp_rgb(double value);
 void yuyv_to_rgb(uint8_t y1, uint8_t u, uint8_t y2, uint8_t v, uint8_t* rgb);
+void yuyv_to_grayscale();
--- src/main.c
+++ src/main.c
@@ -10,11 +10,11 @@
 
 int FRAME_WIDTH = 800;
 int FRAME_HEIGHT = 600;
+int NUM_BUFFERS = 20;
 
-void process_frame(struct buffer* buffer, uint8_t* rgb_data_buffer);
+void process_frame(struct buffer* buffer, struct image_u8* apriltag_image);
 int write_frame_to_file(const char* filename, uint8_t* rgb_data);
-int convert_to_apriltag_image(const uint8_t* buffer, struct image_u8* apriltag_image);
-int detect_apriltag(struct image_u8* apriltag_image, apriltag_detector_t* apriltag_detector);
+int detect_april_tag(struct image_u8* image, apriltag_detector_t* detector);
 
 int main() {
     int camera_fd = setup_camera("/dev/video0", FRAME_WIDTH, FRAME_HEIGHT);
@@ -24,7 +24,7 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    struct v4l2_requestbuffers* request_buffers = request_mmap_buffers(camera_fd, 20);
+    struct v4l2_requestbuffers* request_buffers = request_mmap_buffers(camera_fd, NUM_BUFFERS);
 
     if (request_buffers == NULL) {
         perror("Error while requesting mmap buffers");
@@ -43,34 +43,31 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    uint8_t** rgb_data_buffers = calloc(request_buffers->count, sizeof(uint8_t*));
-    for (int i = 0; i < request_buffers->count; ++i) {
-        rgb_data_buffers[i] = (uint8_t*) malloc(FRAME_WIDTH * FRAME_HEIGHT * 3);
+    struct image_u8** grayscale_image_buffers = calloc(NUM_BUFFERS, sizeof(struct image_u8*));
+    for (int i = 0; i < NUM_BUFFERS; ++i) {
+        grayscale_image_buffers[i] = image_u8_create(FRAME_WIDTH, FRAME_HEIGHT);
     }
 
-    char* filename = (char*) malloc(sizeof(char) * 128);
-    const char* filename_format = "output_%d.ppm";
-
-    struct image_u8* apriltag_image = image_u8_create(FRAME_WIDTH, FRAME_HEIGHT);
     apriltag_family_t *apriltag_family = tag16h5_create();
     apriltag_detector_t *apriltag_detector = apriltag_detector_create();
     apriltag_detector_add_family(apriltag_detector, apriltag_family);
 
     for (int i = 0; i < 10000; ++i) {
-        int dequeued_buffer_index = dequeue_buffer(camera_fd);
-        while (dequeued_buffer_index == -1) {
+        int buffer_index = dequeue_buffer(camera_fd);
+        while (buffer_index == -1) {
             printf("Received -1 response from de-queuing buffer\n");
             perror("");
             usleep(100000);
-            dequeued_buffer_index = dequeue_buffer(camera_fd);
+            buffer_index = dequeue_buffer(camera_fd);
         }
 
-        printf("Dequeued buffer with index: %d\n", dequeued_buffer_index);
+        printf("Dequeued buffer with index: %d\n", buffer_index);
 
-        process_frame(&buffers[dequeued_buffer_index], rgb_data_buffers[dequeued_buffer_index]);
-        snprintf(filename, sizeof(char) * 128, filename_format, i);
-        convert_to_apriltag_image(rgb_data_buffers[dequeued_buffer_index], apriltag_image);
-        int detected_apriltag_id = detect_apriltag(apriltag_image, apriltag_detector);
+        process_frame(&buffers[buffer_index], grayscale_image_buffers[buffer_index]);
+        int detected_apriltag_id = detect_april_tag(
+        grayscale_image_buffers[buffer_index],
+        apriltag_detector
+        );
 
         if (detected_apriltag_id == -1) {
             printf("No april tag detected\n");
@@ -78,12 +75,12 @@ int main() {
             printf("Detected april tag with ID: %d\n", detected_apriltag_id);
         }
 
-        while (requeue_buffer(camera_fd, dequeued_buffer_index) == -1) {
-            printf("WARN: Unable to requeue buffer with index %d\n", dequeued_buffer_index);
+        while (requeue_buffer(camera_fd, buffer_index) == -1) {
+            printf("WARN: Unable to requeue buffer with index %d\n", buffer_index);
             usleep(100000);
         }
 
-        printf("re-queued buffer with index %d\n", dequeued_buffer_index);
+        printf("re-queued buffer with index %d\n", buffer_index);
 
         usleep(100000);
     }
@@ -94,9 +91,9 @@ int main() {
     }
 
     for (int i = 0; i < request_buffers->count; ++i) {
-        free(rgb_data_buffers[i]);
+        free(grayscale_image_buffers[i]);
     }
-    free(rgb_data_buffers);
+    free(grayscale_image_buffers);
     cleanup_buffers(buffers, request_buffers->count);
     free(request_buffers);
     close(camera_fd);
@@ -104,36 +101,43 @@ int main() {
     return 0;
 }
 
-void process_frame(struct buffer* buffer, uint8_t* rgb_data_buffer) {
-    uint8_t *yuyv_ptr = (uint8_t*)buffer->start;
-    size_t yuyv_length = buffer->length;
+// TODO: Write tests for this function using unity
+void process_frame(struct buffer* buffer, struct image_u8* apriltag_image) {
+    uint32_t apriltag_image_size = apriltag_image->stride * apriltag_image->height;
+    int32_t apriltag_image_padding_size = apriltag_image->stride - apriltag_image->width;
+    uint8_t *yuyv_buffer = (uint8_t*)buffer->start;
+    size_t yuyv_buffer_length = buffer->length;
 
-    if (yuyv_length != FRAME_WIDTH * FRAME_HEIGHT * 2) {
-        printf("Could not process frame because yuyv buffer length did not match expected length");
+    if (yuyv_buffer_length != FRAME_WIDTH * FRAME_HEIGHT * 2 || apriltag_image_size * 2 != yuyv_buffer_length) {
+        printf("Could not process frame because of improper buffer lengths\n"
+               "Actual YUYV Buffer Length: %zu\nExpected YUYV Buffer Length: %d\nAprilTag Image Buffer Length: %d\n",
+               yuyv_buffer_length, FRAME_WIDTH * FRAME_HEIGHT * 2, apriltag_image_size);
         return;
     }
 
-    for (size_t i = 0; i < yuyv_length; i += 4) {
-        yuyv_to_rgb(yuyv_ptr[i], yuyv_ptr[i + 1], yuyv_ptr[i + 2], yuyv_ptr[i + 3], &rgb_data_buffer[(i / 4) * 6]);
+    for (size_t i = 0, j = 0; i < yuyv_buffer_length; i += 4, j += 2) {
+        apriltag_image->buf[j] = yuyv_buffer[i];
+        apriltag_image->buf[j+1] = yuyv_buffer[i + 2];
+
+        if (((j + 2) % apriltag_image->stride) == 0) {
+            memset(apriltag_image->buf, 0, apriltag_image_padding_size);
+            j += apriltag_image_padding_size;
+        }
     }
 }
 
 int write_frame_to_file(const char* filename, uint8_t* rgb_data) {
-    // Open the file for writing
     FILE *file = fopen(filename, "wb");
     if (!file) {
         perror("Unable to open file for writing");
         return -1;
     }
 
-    // Write the PPM header
-    // P6 is the magic number for PPM binary format
-    // Then the image width and height and the maximum color value (255 for 8 bits per channel)
-    fprintf(file, "P6\n%d %d\n255\n", FRAME_WIDTH, FRAME_HEIGHT);
+    int maximum_color_value = 255;
+    fprintf(file, "P6\n%d %d\n%d\n", FRAME_WIDTH, FRAME_HEIGHT, maximum_color_value);
 
-    // Write the RGB data
-    size_t written = fwrite(rgb_data, 1, FRAME_WIDTH * FRAME_HEIGHT * 3, file);
-    if (written != FRAME_WIDTH * FRAME_HEIGHT * 3) {
+    size_t bytes_written = fwrite(rgb_data, 1, FRAME_WIDTH * FRAME_HEIGHT * 3, file);
+    if (bytes_written != FRAME_WIDTH * FRAME_HEIGHT * 3) {
         perror("Error writing RGB data to file");
         fclose(file);
         return -1;
@@ -165,9 +169,9 @@ int is_valid_id(int id) {
     return id >= 1 && id < 9;
 }
 
-int detect_apriltag(struct image_u8* apriltag_image, apriltag_detector_t* td) {
+int detect_april_tag(struct image_u8* image, apriltag_detector_t* detector) {
     int detected_tag_id = -1;
-    zarray_t* detections = apriltag_detector_detect(td, apriltag_image);
+    zarray_t* detections = apriltag_detector_detect(detector, image);
     for (int i = 0; i < zarray_size(detections); ++i) {
         apriltag_detection_t* detection;
         zarray_get(detections, i, &detection);
